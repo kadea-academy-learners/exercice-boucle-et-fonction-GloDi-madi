774cbb1797c9ebfaea07750b2764e4184b3aee77
GLoDI-madi <GLoDImadingu12@hotmail.com>
2025-11-02 04:06:33 +0000

    mon troisieme commit arrayAverage

---
+++ .history/02-array-average_20251102045003.js
@@ -0,0 +1,14 @@
+// Crée un tableau `notes` contenant 5 nombres
+// Écrire une fonction averageNote avec `notes` comme parametre qui effectue les opérations suivantes :
+// - Calcule la moyenne des notes
+// - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
+
+function averageNote(notes) {
+    const sum = notes.reduce((acc, note) => acc + note, 0);
+	const average = sum / notes.length;	
+
+}
+
+module.exports = {
+	averageNote,
+};
---
+++ .history/02-array-average_20251102045543.js
@@ -0,0 +1,20 @@
+// Crée un tableau `notes` contenant 5 nombres
+// Écrire une fonction averageNote avec `notes` comme parametre qui effectue les opérations suivantes :
+// - Calcule la moyenne des notes
+// - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
+
+function averageNote(notes) {
+    const sum = notes.reduce((acc, note) => acc + note, 0);
+	const average = sum / notes.length;
+	if (average >= 10) {
+		return "Réussi";
+	} else {
+		return "Échoué";
+	}
+
+   console.log(average);
+}
+
+module.exports = {
+	averageNote,
+};
